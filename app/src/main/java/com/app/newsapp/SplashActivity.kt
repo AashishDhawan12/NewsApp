b5250767e96a0e20c25490ed8305170ff4c595a6
AashishDhawan12 <dh.aashish2003@gmail.com>
2025-02-21 17:41:39 +0530

    Splash Activity added

---
+++ app/src/main/java/com/app/newsapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.app.newsapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_activity)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },700)
+    }
+}
